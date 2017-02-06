eba559c25939e37da73df71b25ac9d3e40ebe3e4
Alex-Diez <alexvic.amatory@gmail.com>
2017-02-06 19:34:51 +0200

    bowling game kata third day

---
+++ bowling-kata/day-3/kata.js
@@ -0,0 +1,50 @@
+var BowlingGame = function() {
+    this.rolls = [];
+    this.current = 0;
+};
+
+BowlingGame.prototype.roll = function(pins) {
+    this.rolls[this.current++] = pins;
+};
+
+BowlingGame.prototype.score = function() {
+    var frameIndex = 0;
+    var score = 0;
+    var self = this;
+
+    function isStrike() {
+        return self.rolls[frameIndex] === 10;
+    }
+
+    function strikePoints() {
+        return 10 + self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2]
+    }
+
+    function isSpare() {
+        return framePoints() === 10;
+    }
+
+    function sparePoints() {
+        return framePoints() + self.rolls[frameIndex + 2]
+    }
+
+    function framePoints() {
+        return self.rolls[frameIndex] + self.rolls[frameIndex + 1]
+    }
+
+    for(var frame = 0; frame < 10; frame++) {
+        if (isStrike()) {
+            score += strikePoints();
+            frameIndex += 1;
+        } else if (isSpare()) {
+            score += sparePoints();
+            frameIndex += 2;
+        } else {
+            score += framePoints();
+            frameIndex += 2;
+        }
+    }
+    return score;
+};
+
+module.exports = BowlingGame;
