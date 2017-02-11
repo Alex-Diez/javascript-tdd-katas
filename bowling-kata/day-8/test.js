9e77ab5fc127a8ea02479e776e793c48c2fd3647
Alex-Diez <alexvic.amatory@gmail.com>
2017-02-11 12:46:36 +0200

    bowling game kata eighth day

---
+++ bowling-kata/day-8/test.js
@@ -0,0 +1,55 @@
+const BowlingGame = require("./kata"); 
+
+describe("bowling game", () => {
+    var game;
+
+    beforeEach(() => {
+        game = new BowlingGame();
+    });
+
+    function rollMany(times, pins) {
+        for(var i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    test("gutter game", () => {
+        rollMany(20, 0);
+
+        expect(game.score()).toBe(0);
+    });
+
+    test("all ones", () => {
+        rollMany(20, 1);
+
+        expect(game.score()).toBe(20);
+    });
+
+    function rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
+    test("one spare", () => {
+        rollSpare();
+        game.roll(3);
+        rollMany(17, 0);
+
+        expect(game.score()).toBe(16);
+    });
+
+    test("one strike", () => {
+        game.roll(10);
+        game.roll(4);
+        game.roll(3);
+        rollMany(16, 0);
+
+        expect(game.score()).toBe(24);
+    });
+
+    test("perfect game", () => {
+        rollMany(13, 10);
+
+        expect(game.score()).toBe(300);
+    });
+});
