1baf915f8aa6b6c233692930c309350a80b46a29
Alex-Diez <alex.dukhno@icloud.com>
2017-03-02 20:40:14 +0200

    stack kata seventh day

---
+++ stack-kata/day-7/test.js
@@ -0,0 +1,120 @@
+const Stack = require("./kata");
+
+describe("stack kata", () => {
+    test("create stack", () => {
+        var stack = new Stack();
+    });
+
+    test("push one", () => {
+        var stack = new Stack();
+
+        stack.push(1);
+
+        expect(stack.pop()).toBe(1);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("push many", () => {
+        var stack = new Stack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        expect(stack.pop()).toBe(3);
+        expect(stack.pop()).toBe(2);
+        expect(stack.pop()).toBe(1);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("for each", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        var sum = 0;
+
+        stack.forEach(function(item) {
+            sum += item;
+        });
+
+        expect(sum).toBe(6);
+    });
+
+    test("pop - push for each", () => {
+        var stack = new Stack();
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        var sum = 0;
+        stack.forEach(function(item) {
+            sum += item;
+        });
+
+        expect(sum).toBe(6);
+    });
+
+    test("map over stack", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        stack.map(function(item) {
+            return item * item;
+        });
+
+        expect(stack.pop()).toBe(9);
+        expect(stack.pop()).toBe(4);
+        expect(stack.pop()).toBe(1);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("create iterator from stack", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        var iter = stack.iter();
+
+        expect(iter.next()).toBe(3);
+        expect(iter.next()).toBe(2);
+        expect(iter.next()).toBe(1);
+        expect(iter.next()).toBe(undefined);
+    });
+
+    test("create two iterators from stack", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+        
+        var iter = stack.iter();
+
+        expect(iter.next()).toBe(3);
+        expect(iter.next()).toBe(2);
+        expect(iter.next()).toBe(1);
+        expect(iter.next()).toBe(undefined);
+        var iter = stack.iter();
+
+        expect(iter.next()).toBe(3);
+        expect(iter.next()).toBe(2);
+        expect(iter.next()).toBe(1);
+        expect(iter.next()).toBe(undefined);
+    });
+
+    test("iterator from stack that populated with pushes", () => {
+        var stack = new Stack();
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        var iter = stack.iter();
+
+        expect(iter.next()).toBe(3);
+        expect(iter.next()).toBe(2);
+        expect(iter.next()).toBe(1);
+        expect(iter.next()).toBe(undefined);
+    });
+});
