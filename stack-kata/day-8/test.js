93940faba9b88d73b482081a90b6f3351156c33d
Alex-Diez <alex.dukhno@icloud.com>
2017-03-03 22:51:21 +0200

    stack kata eighth day

---
+++ stack-kata/day-8/test.js
@@ -0,0 +1,64 @@
+const Stack = require("./kata");
+
+describe("stack kata", () => {
+    test("create stack", () => {
+        var stack = new Stack();
+    });
+
+    test("push one", () => {
+        var stack = new Stack();
+
+        stack.push(1);
+
+        expect(stack.pop()).toBe(1);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("push many", () => {
+        var stack = new Stack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        expect(stack.pop()).toBe(3);
+        expect(stack.pop()).toBe(2);
+        expect(stack.pop()).toBe(1);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("for each", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        var sum = 0;
+        stack.forEach(function(item) {
+            sum += item;
+        });
+
+        expect(sum).toBe(6);
+    });
+
+    test("map over stack", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        stack.map(function(item) {
+            return item * 2;
+        });
+
+        expect(stack.pop()).toBe(6);
+        expect(stack.pop()).toBe(4);
+        expect(stack.pop()).toBe(2);
+        expect(stack.pop()).toBe(undefined);
+    });
+
+    test("iterator over stack", () => {
+        var stack = new Stack(new Array(1, 2, 3));
+
+        var iter = stack.iter();
+
+        expect(iter.next()).toBe(3);
+        expect(iter.next()).toBe(2);
+        expect(iter.next()).toBe(1);
+        expect(iter.next()).toBe(undefined);
+    });
+});
