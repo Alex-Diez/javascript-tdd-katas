4556a4e6ec4fdb7dc4406e3916dd884f22858e92
Alex-Diez <alex.dukhno@icloud.com>
2017-03-19 14:04:48 +0200

    react label kata ninth day

---
+++ react-katas/label-kata/day-9/kata.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class CheckboxWithLabel extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {checked: false};
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange() {
+        this.setState({checked: !this.state.checked});
+    }
+
+    render() {
+        return(
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={this.onChange}
+                    checked={this.state.checked}
+                />
+                {this.state.checked ? 'On' : 'Off'}
+            </label>
+        )
+    }
+}
