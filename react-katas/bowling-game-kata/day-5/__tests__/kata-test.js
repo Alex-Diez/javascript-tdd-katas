32e35f1c5a9886252b92dc1dd0c73cbfc156bff6
Alex-Diez <alex.dukhno@icloud.com>
2017-04-04 20:24:22 +0300

    react bowling game kata fifth day

---
+++ react-katas/bowling-game-kata/day-5/__tests__/kata-test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import BowlingGame from '../kata';
+
+describe("bowling game", () => {
+    let bowlingGame;
+    let rollInput;
+    let rollButton;
+    let scoreButton;
+
+    beforeEach(() => {
+        bowlingGame = TestUtils.renderIntoDocument(
+            <BowlingGame/>
+        );
+        rollInput = TestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'input');
+        rollButton = TestUtils.findRenderedDOMComponentWithClass(bowlingGame, 'roll-button');
+        scoreButton = TestUtils.findRenderedDOMComponentWithClass(bowlingGame, 'score-button');
+    });
+
+    function rollMany(times, pins) {
+        for (let i = 0; i < times; i++) {
+            rollInput.value = pins;
+            TestUtils.Simulate.change(rollInput);
+            TestUtils.Simulate.click(rollButton);
+        }
+    }
+
+    function calculateScore() {
+        TestUtils.Simulate.click(scoreButton);
+    }
+
+    function getScore() {
+        return Number(TestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'span').textContent);
+    }
+
+    function rollSpare() {
+        rollInput.value = 5;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+        rollInput.value = 5;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+    }
+
+    test("gutter game", () => {
+        rollMany(20, 0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(0);
+    });
+
+    test("roll all ones", () => {
+        rollMany(20, 1);
+
+        calculateScore();
+
+        expect(getScore()).toBe(20);
+    });
+
+    test("roll one spare", () => {
+        rollSpare();
+
+        rollInput.value = 3;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+
+        rollMany(17, 0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(16);
+    });
+
+    test("roll one strike", () => {
+        rollInput.value = 10;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+
+        rollInput.value = 4;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+        rollInput.value = 3;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+
+        rollMany(16, 0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(24);
+    });
+});
