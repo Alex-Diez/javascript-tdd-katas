15ce8da08252d3a8933471f7bda615d5a13f6e21
Alex-Diez <alex.dukhno@icloud.com>
2017-04-08 15:51:46 +0300

    react bowling game kata ninth day

---
+++ react-katas/bowling-game-kata/day-9/__tests__/kata-test.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import BowlingGame from '../kata';
+
+describe("bowling game", () => {
+    let bowlingGame;
+    let rollInput;
+    let rollButton;
+    let scoreButton;
+
+    beforeEach(() => {
+        bowlingGame = TestUtils.renderIntoDocument(
+            <BowlingGame/>
+        );
+        rollInput = TestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'input');
+        rollButton = TestUtils.findRenderedDOMComponentWithClass(bowlingGame, 'roll-button');
+        scoreButton = TestUtils.findRenderedDOMComponentWithClass(bowlingGame, 'score-button');
+    });
+
+    function calculateScore() {
+        TestUtils.Simulate.click(scoreButton);
+    }
+
+    function rollMany(times, pins) {
+        for (let i = 0; i < times; i++) {
+            rollInput.value = pins;
+            TestUtils.Simulate.change(rollInput);
+            TestUtils.Simulate.click(rollButton);
+        }
+    }
+
+    function getScore() {
+        return Number(TestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'span').textContent);
+    }
+
+    function rollSpare() {
+        rollInput.value = 5;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+        rollInput.value = 5;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+    }
+
+    function rollStrike() {
+        rollInput.value = 10;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+    }
+
+    test("gutter game", () => {
+        rollMany(20, 0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(0);
+    });
+
+    test("roll all ones", () => {
+        rollMany(20, 1);
+
+        calculateScore();
+
+        expect(getScore()).toBe(20);
+    });
+
+    test("roll one spare", () => {
+        rollSpare();
+
+        rollInput.value = 3;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+
+        rollMany(17 ,0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(16);
+    });
+
+    test("roll one strike", () => {
+        rollStrike();
+
+        rollInput.value = 3;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+        rollInput.value = 4;
+        TestUtils.Simulate.change(rollInput);
+        TestUtils.Simulate.click(rollButton);
+
+        rollMany(16, 0);
+
+        calculateScore();
+
+        expect(getScore()).toBe(24);
+    });
+
+    test("perfect game", () => {
+        rollMany(13, 10);
+
+        calculateScore();
+
+        expect(getScore()).toBe(300);
+    });
+});
---
+++ react-katas/bowling-game-kata/day-9/kata.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+export default class BowlingGame extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.props = props;
+        this.state = {
+            score: 0,
+            currentRoll: 0,
+            rolls: []
+        };
+
+        this.onRoll = this.onRoll.bind(this);
+        this.roll = this.roll.bind(this);
+        this.score = this.score.bind(this);
+    }
+
+    onRoll(e) {
+        e.preventDefault();
+        this.setState({currentRoll: Number(e.target.value)});
+    }
+
+    roll(e) {
+        e.preventDefault();
+        const rolls = this.state.rolls;
+        rolls.push(this.state.currentRoll);
+        this.setState({rolls: rolls});
+    }
+
+    score(e) {
+        e.preventDefault();
+
+        let score = 0;
+        let rolls = this.state.rolls;
+        let frameIndex = 0;
+
+        function isSpare() {
+            return rolls[frameIndex] + rolls[frameIndex + 1] === 10;
+        }
+
+        function spareBonus() {
+            return rolls[frameIndex + 2];
+        }
+
+        function isStrike() {
+            return rolls[frameIndex] === 10;
+        }
+
+        function strikeBonus() {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        for (let frame = 0; frame < 10; frame++) {
+            if (isStrike()) {
+                score += 10 + strikeBonus();
+                frameIndex += 1;
+            } else if (isSpare()) {
+                score += 10 + spareBonus();
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
+
+        this.setState({score: score});
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    onChange={this.onRoll}
+                />
+                <button
+                    className="roll-button"
+                    onClick={this.roll}
+                >
+                    roll
+                </button>
+                <button
+                    className="score-button"
+                    onClick={this.score}
+                >
+                    score
+                </button>
+                <span>{this.state.score}</span>
+            </form>
+        )
+    }
+}
