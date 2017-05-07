ea7c84379e7fa79cdc4223c384233825a655f7f2
Alex-Diez <alex.dukhno@icloud.com>
2017-05-07 12:36:45 +0300

    react roman number seventh day

---
+++ react-katas/roman-number-kata/day-7/kata.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+
+export default class Converter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.props = props;
+        this.state = {
+            conversionMap: new Map([[1, 'I'], [5, 'V'], [4, 'IV']]),
+            arabic: 0,
+            result: ''
+        };
+
+        this.onInput = this.onInput.bind(this);
+        this.convert = this.convert.bind(this);
+    }
+
+    onInput(e) {
+        e.preventDefault();
+
+        this.setState({
+            arabic: Number(e.target.value)
+        })
+    }
+
+    convert(e) {
+        e.preventDefault();
+
+        const conversionMap = this.state.conversionMap;
+        let number = this.state.arabic;
+
+        function convert() {
+            if (number < 1) {
+                return "";
+            }
+            const arabic = Array.from(conversionMap.keys()).filter((item) => item <= number).sort().pop();
+            const roman = conversionMap.get(arabic);
+            number -= arabic;
+            return roman + convert();
+        }
+
+        this.setState({
+            result: convert()
+        })
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    onChange={this.onInput}
+                />
+                <button
+                    onClick={this.convert}
+                >
+                    convert
+                </button>
+                <span>{this.state.result}</span>
+            </form>
+        )
+    }
+}
