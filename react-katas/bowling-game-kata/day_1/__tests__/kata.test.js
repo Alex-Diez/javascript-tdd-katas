15274d3a5466ed61403ec51d14db39e4f7399e7f
Alex-Diez <alex.dukhno@icloud.com>
2017-11-08 22:20:51 +0200

    bowling game kata first day

--- react-katas/bowling-game-kata/day_1/__tests__/kata.test.js
+++ react-katas/bowling-game-kata/day_1/__tests__/kata.test.js
@@ -1,1 +1,65 @@
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 
+import BowlingGame from '../kata';
+
+describe('bowling game kata tests', () => {
+    const rollMany = (times, pin) => {
+        for(let i = 0; i < times; i++) {
+            rollOnce(pin);
+        }
+    };
+
+    const rollOnce = (pin) => {
+        pinInput.value = pin;
+        ReactTestUtils.Simulate.change(pinInput);
+        ReactTestUtils.Simulate.click(roll);
+    };
+
+    const rollSpare = () => {
+        rollOnce(4);
+        rollOnce(6);
+    };
+
+    const rollStrike = () => rollOnce(10);
+
+    let pinInput;
+    let roll;
+    let score;
+
+    beforeEach(() => {
+        const bowlingGame = ReactTestUtils.renderIntoDocument(<BowlingGame/>);
+        pinInput = ReactTestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'input');
+        roll = ReactTestUtils.findRenderedDOMComponentWithTag(bowlingGame, 'button');
+        score = ReactTestUtils.findRenderedDOMComponentWithClass(bowlingGame, 'score');
+    });
+
+    test('gutter game', () => {
+        rollMany(20, 0);
+
+        expect(Number(score.textContent)).toBe(0);
+    });
+
+    test('all ones', () => {
+        rollMany(20, 1);
+
+        expect(Number(score.textContent)).toBe(20);
+    });
+
+    test('one spare', () => {
+        rollSpare();
+        rollOnce(3);
+        rollMany(17, 0);
+
+        expect(Number(score.textContent)).toBe(16);
+    });
+
+    test('one strike', () => {
+        rollStrike();
+        rollOnce(3);
+        rollOnce(4);
+        rollMany(16, 0);
+
+        expect(Number(score.textContent)).toBe(24);
+    });
+});
--- react-katas/bowling-game-kata/day_1/kata.js
+++ react-katas/bowling-game-kata/day_1/kata.js
@@ -1,1 +1,60 @@
+import React, {Component} from 'react';
 
+export default class BowlingGame extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rolls: [],
+            score: 0
+        };
+
+        this.enterPin = this.enterPin.bind(this);
+        this.roll = this.roll.bind(this);
+    }
+
+    enterPin(e) {
+        const rolls = this.state.rolls;
+        rolls.push(Number(e.target.value));
+        this.setState({rolls: rolls});
+
+        e.preventDefault();
+    }
+
+    roll(e) {
+        let score = 0;
+        let frameIndex = 0;
+        const rolls = this.state.rolls;
+
+        const isStrike = (frameIndex) => rolls[frameIndex] === 10;
+        const strikeBonus = (frameIndex) => rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        const isSpare = (frameIndex) => rolls[frameIndex] + rolls[frameIndex + 1] === 10;
+        const spareBonus = (frameIndex) => rolls[frameIndex + 2];
+
+        for (let i = 0; i < 10; i++) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex += 1;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
+
+        this.setState({score: score});
+
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form>
+                <input onChange={this.enterPin}/>
+                <button onClick={this.roll}/>
+                <span className="score">{this.state.score}</span>
+            </form>
+        )
+    }
+}
