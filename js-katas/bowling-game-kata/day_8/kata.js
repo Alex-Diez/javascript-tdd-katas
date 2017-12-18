d3c9a6d04f8ea48733f82f24caea09c1c6a1b147
Alex-Diez <alex.dukhno@icloud.com>
2017-12-18 22:00:56 +0200

    bowling game kata eighth day

---
+++ js-katas/bowling-game-kata/day_8/kata.js
@@ -0,0 +1,47 @@
+const BowlingGame = function () {
+    this.rolls = [];
+};
+
+BowlingGame.prototype.roll = function (pin) {
+    this.rolls.push(pin);
+};
+
+BowlingGame.prototype.score = function () {
+    const self = this;
+
+    function isStrike(frameIndex) {
+        return self.rolls[frameIndex] === 10;
+    }
+
+    function strikeBonus(frameIndex) {
+        return self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2];
+    }
+
+    function isSpare(frameIndex) {
+        return self.rolls[frameIndex] + self.rolls[frameIndex + 1] === 10
+    }
+
+    function spareBonus(frameIndex) {
+        return self.rolls[frameIndex + 2]
+    }
+
+    let score = 0;
+    let frameIndex = 0;
+
+    for (let _ of new Array(10)) {
+        if (isStrike(frameIndex)) {
+             score += 10 + strikeBonus(frameIndex);
+             frameIndex += 1;
+        } else if (isSpare(frameIndex)) {
+            score += 10 + spareBonus(frameIndex);
+            frameIndex += 2;
+        } else {
+            score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+            frameIndex += 2;
+        }
+    }
+
+    return score;
+};
+
+export {BowlingGame};
