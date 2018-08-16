f9e8959b8799ed205dfd3bf123e4e7941b524910
Alex-Diez <alex.dukhno@icloud.com>
2018-08-16 16:46:01 +0300

    bowling game kata second day

---
+++ js-katas/bowling-game-kata/iteration-2/day_2/__tests__/kata.test.js
@@ -0,0 +1,60 @@
+import { BowlingGame } from '../kata';
+import { should } from 'chai';
+
+should();
+
+describe('bowling game', () => {
+  beforeAll(() => {
+    BowlingGame.prototype.rollMany = function (times, pins) {
+      Array.from(Array(times)).forEach((_) => this.roll(pins))
+    };
+    BowlingGame.prototype.rollSpare = function () {
+      this.roll(4);
+      this.roll(6);
+    };
+    BowlingGame.prototype.rollStrike = function () {
+      this.roll(10);
+    }
+  });
+
+  let game;
+
+  beforeEach(() => {
+    game = new BowlingGame();
+  });
+
+  it('all zeros', () => {
+    game.rollMany(20, 0);
+
+    game.score().should.eq(0);
+  });
+
+  it('all ones', () => {
+    game.rollMany(20, 1);
+
+    game.score().should.eq(20);
+  });
+
+  it('one spare', () => {
+    game.rollSpare();
+    game.roll(3);
+    game.rollMany(17, 0);
+
+    game.score().should.eq(10 + 3 + 3);
+  });
+
+  it('one strike', () => {
+    game.rollStrike();
+    game.roll(4);
+    game.roll(3);
+    game.rollMany(16, 0);
+
+    game.score().should.eq(10 + 4 + 3 + 4 + 3);
+  });
+
+  it('perfect game', () => {
+    game.rollMany(12, 10);
+
+    game.score().should.eq(300);
+  });
+});
