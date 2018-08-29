8bef5f89a051154cb1dfa30d5c3e0009006e2670
Alex-Diez <alex.dukhno@icloud.com>
2018-08-29 10:54:14 +0300

    bowling game fourteenth day

---
+++ js-katas/bowling-game-kata/iteration-2/day_14/__tests__/kata.test.js
@@ -0,0 +1,57 @@
+import { should } from 'chai';
+
+import { BowlingGame } from '../kata';
+
+should();
+
+describe('bowling game', () => {
+  let game;
+
+  beforeAll(() => {
+    BowlingGame.prototype.rollMany = function(times, pins) { Array.from(Array(times)).forEach((_) => this.roll(pins)) };
+    BowlingGame.prototype.rollSpare = function() {
+      this.roll(4);
+      this.roll(6);
+    };
+    BowlingGame.prototype.rollStrike = function() { this.roll(10) };
+  });
+
+  beforeEach(() => {
+    game = new BowlingGame();
+  });
+
+  it('all zeros', () => {
+    game.rollMany(20, 0);
+
+    game.score().should.eq(0);
+  });
+
+  it('all ones', () => {
+    game.rollMany(20, 1);
+
+    game.score().should.eq(20);
+  });
+
+  it('one spare', () => {
+    game.rollSpare();
+    game.roll(3);
+    game.rollMany(17, 0);
+
+    game.score().should.eq(10 + 3 + 3);
+  });
+
+  it('one strike', () => {
+    game.rollStrike();
+    game.roll(4);
+    game.roll(3);
+    game.rollMany(16, 0);
+
+    game.score().should.eq(10 + 3 + 4 + 3 + 4);
+  });
+
+  it('perfect game', () => {
+    game.rollMany(12, 10);
+
+    game.score().should.eq(300);
+  });
+});
---
+++ js-katas/bowling-game-kata/iteration-2/day_14/kata.js
@@ -0,0 +1,32 @@
+const BowlingGame = function() {
+  this.rolls = [];
+};
+
+BowlingGame.prototype.roll = function(pins) {
+  this.rolls.push(pins);
+};
+
+BowlingGame.prototype.score = function() {
+  const isSpare = (roll) => this.rolls[roll] + this.rolls[roll + 1] === 10;
+  const spareBonus = (roll) => this.rolls[roll + 2];
+  const isStrike = (roll) => this.rolls[roll] === 10;
+  const strikeBonus = (roll) => this.rolls[roll + 1] + this.rolls[roll + 2];
+
+  let score = 0;
+  let roll = 0;
+  for (let i = 0; i < 10; i++) {
+    if (isStrike(roll)) {
+      score += 10 + strikeBonus(roll);
+      roll += 1;
+    } else if (isSpare(roll)) {
+      score += 10 + spareBonus(roll);
+      roll += 2;
+    } else {
+      score += this.rolls[roll] + this.rolls[roll + 1];
+      roll += 2;
+    }
+  }
+  return score;
+}
+
+module.exports.BowlingGame = BowlingGame;
