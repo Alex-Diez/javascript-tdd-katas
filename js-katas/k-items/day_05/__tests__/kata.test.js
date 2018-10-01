5edaf03f03b1b68c297fe6ca7269e1f2e0a9ef92
Alex-Diez <alex.dukhno@icloud.com>
2018-10-01 14:43:29 +0300

    k items kata fifth day

---
+++ js-katas/k-items/day_05/__tests__/kata.test.js
@@ -0,0 +1,32 @@
+import {should} from 'chai';
+
+import Filter from '../kata';
+
+should();
+
+describe('k items', () => {
+  it('has to filter one item from single item array', () => {
+    return Filter.kItems([1], 1)
+        .then(({numbers}) => numbers.should.have.members([1]));
+  });
+
+  it('has to filter one items from two items array largest is first', () => {
+    return Filter.kItems([2, 1], 1)
+        .then(({numbers}) => numbers.should.have.members([2]));
+  });
+
+  it('has to filter one item from two items array largest is last', () => {
+    return Filter.kItems([1, 2], 1)
+        .then(({numbers}) => numbers.should.have.members([2]));
+  });
+
+  it('has to filter one item from ordered three items array', () => {
+    return Filter.kItems([1, 2, 3], 1)
+        .then(({numbers}) => numbers.should.have.members([3]));
+  });
+
+  it('has to filter two items from ordered three items array', () => {
+    return Filter.kItems([1, 2, 3], 2)
+        .then(({numbers}) => numbers.should.have.members([2, 3]));
+  })
+});
