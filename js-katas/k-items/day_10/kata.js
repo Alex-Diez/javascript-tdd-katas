dab10e7e6fe3925e06bf86faa79fe04d615b3793
Alex-Diez <alex.dukhno@icloud.com>
2018-10-07 15:48:38 +0300

    k items kata tenth day

---
+++ js-katas/k-items/day_10/kata.js
@@ -0,0 +1,18 @@
+export default class Filter {
+  static async kItems(items, k) {
+    let max = [];
+    for (let i = 0; i < k; i++) {
+      max[i] = items[i];
+    }
+    for (let i = k; i < items.length; i += 1) {
+      let n = 0;
+      while (n < k && items[i] <= max[n]) {
+        n += 1;
+      }
+      if (n < k) {
+        max[n] = items[i];
+      }
+    }
+    return {numbers: max};
+  }
+}
